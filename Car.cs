186430732b08f2299d94c10add54c3f58acad899
eliza <eliza@DESKTOP-HLLIIPJ>
2022-11-09 10:15:58 +0200

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+class Car
+{
+    private string name;
+    private string color;
+    private double price;
+
+    //Властивысть доступу до поля колір
+    public string Color
+    {
+        get { return color; }
+        set { color = value; }
+    }
+
+    //За замовчуванням
+    public Car() : this("Name", "Color", 10000)
+    {
+    }
+
+    //Конструктор з параметрами
+    public Car(string name, string color, int price)
+    {
+        this.name = name;
+        this.color = color;
+        this.price = price;
+    }
+
+    //Введення даних про авто
+    public void Input()
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+        Console.WriteLine("Введіть марку машини");
+        this.name = Console.ReadLine();
+        Console.WriteLine("Введіть колір машини");
+        this.color = Console.ReadLine();
+        Console.WriteLine("Введіть вартість машини");
+        this.price = Convert.ToDouble(Console.ReadLine());
+    }
+
+    //Виведення даних про авто
+    public void Print()
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Console.InputEncoding = System.Text.Encoding.Unicode;
+
+        Console.WriteLine($"Name: {this.name} Color: {this.color} Price: {this.price}");
+    }
+
+    //Перевірка чи збігається марка та колір
+    public static bool operator ==(Car car1, Car car2)
+    {
+        return (car1.name == car2.name) && (car1.price == car2.price);
+    }
+    public static bool operator !=(Car car1, Car car2)
+    {
+        return (car1.name != car2.name) && (car1.price != car2.price);
+    }
+
+    //Виведення даних про авто за допомогою ToString
+    public override string ToString()
+    {
+        return $"Name: {this.name} Color: {this.color} Price: {this.price}";
+    }
+
+    //Додає х% до ціни
+    public void ChangePrice(double x)
+    {
+        price += price * x / 100;
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лабораторна_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+
+            Console.WriteLine("Натисніть будь-яку кнопку, для того, щоб вивести дані на екран");
+            Console.ReadKey();
+
+            //Cars
+            Car car1 = new Car("Volkswagen", "Gray", 7000);
+            Car car2 = new Car("Nissan", "Black", 10000);
+            Car car3 = new Car("Toyota", "Blue", 15000);
+
+            car1.Print();
+            car1.ChangePrice(10);
+            car1.Print();
+
+            car2.Print();
+            car2.ChangePrice(10);
+            car2.Print();
+
+            car3.Print();
+            car3.ChangePrice(10);
+            car3.Print();
+
+            Console.WriteLine(car1 == car2);
+            Console.WriteLine(car2 == car3);
+
+            Console.ReadKey();
+        }
+
+    }
+}
+   
